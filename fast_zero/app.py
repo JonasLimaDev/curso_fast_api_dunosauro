a1f848a1ea98aff07e3c3cc574490e62a9dd8c32
Jonas Lima <jonas.lima.dev@gmail.com>
2024-09-23 12:05:32 -0300

    Adição de Schema para documentação Automática da API

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fast_zero.schemas import Massage
+from http import HTTPStatus
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Massage)
 def read_root():
     return {"message": "Olá Mundo"}
---
+++ fast_zero/schemas.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+
+class Massage(BaseModel):
+    massage: str
